4abbdc733df52511fbf505e436fb81d9a59c4f44
astral-wayfarer727 <astralwayfarer727@gmail.com>
2026-09-03 15:49:24 +0100

    0.2.1a

--- src/main/java/astro/dimensional_additions/items/DAFoodComponents.java
+++ src/main/java/astro/dimensional_additions/items/DAFoodComponents.java
@@ -3,5 +3,5 @@ package astro.dimensional_additions.items;
 import net.minecraft.component.type.FoodComponent;
 
 public class DAFoodComponents {
-    public static final FoodComponent DRIFT_BERRY = new FoodComponent.Builder().nutrition(2).saturationModifier(0.1F).build();
+    public static final FoodComponent DRIFT_BERRY = new FoodComponent.Builder().nutrition(2).saturationModifier(0.1F).alwaysEdible().build();
 }
